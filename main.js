01a38ce5b0fad19699d28e8b6471e8ee7acb177c
sungyean Hong <hosuye@gmail.com>
2025-09-09 00:07:07 +0900

    9

--- main.js
+++ main.js
@@ -41,13 +41,11 @@ function play() {
   } else {
     resultArea.textContent = "맞췄습니다.!";
     gameOver = true;
-
   }
 
   history.push(userValue);
   if (chance < 1) {
     gameOver = true;
-
   }
 
   if (gameOver == true) {
@@ -58,12 +56,11 @@ function play() {
 function reset() {
   inputArea.value = "";
   pickRandomnum();
+  history = []
   buttonGo = false;
   chance = 3
   chanceArea.textContent = `남은 기회는: ${chance}번`;
 
-
-  history = []
   resultArea.textContent = "결과값이 여기 나옵니다."
 
 
