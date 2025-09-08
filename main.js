d7452b8472326ff0b92a21785d2ffcda1eb5cf2c
sungyean Hong <hosuye@gmail.com>
2025-09-09 00:22:45 +0900

    9

--- main.js
+++ main.js
@@ -55,13 +55,12 @@ function play() {
 
 function reset() {
   inputArea.value = "";
-
   pickRandomnum();
   chance = 3;
   chanceArea.textContent = `남은 기회는: ${chance}번`;
-  if (reset == true) {
-    buttonGo.disabled = false;
-  }
+  history = []
+  buttonGo.disabled = false;
+
   resultArea.textContent = "결과값이 여기 나옵니다."
 
 
