e059d2bc200d0e4a984438dfffb28c739df966f6
hong-free <hosuye@gmail.com>
2025-09-10 23:41:51 +0900

    3

--- todo_list/main.js
+++ todo_list/main.js
@@ -4,23 +4,59 @@ let taskList = []
 addButton.addEventListener("click", addTask);
 
 function addTask() {
-  let taskContent = taskInput.value
-  taskList.push(taskContent);
+  let task = {
+    id: randomIDGenerate(),
+    taskContent: taskInput.value,
+    isComplete: false
+  }
+  taskList.push(task);
+
   render();
 }
 
 function render() {
   let resultHtml = "";
   for (let i = 0; i < taskList.length; i++) {
-    resultHtml += `<div class="task">
-      <div>${taskList[i]}</div>
+    if (taskList[i].isComplete == true) {
+      resultHtml += `<div class="task">
+      <div class="task-done">${taskList[i].taskContent}</div>
         <div>
-          <button>check</button>
-          <button>Clear</button>
+          <button onclick="toggleComplete(`${ taskList[i].id } `)">Check</button>
+          <button onclick="deleteTask(`${ taskList[i].id } `) ">Delete</button>
+        </div >
+        </div > `
+    } else {
+      resultHtml += `< div class="task" >
+      <div>${taskList[i].taskContent}</div>
+        <div>
+          <button onclick= "toggleComplete(`${ taskList[i].id } ` )">Check</button>
+          <button onclick="deleteTask(`${ taskList[i].id } `)">Delete</button>
         </div>
-        </div>`;
+        <div/ > `
+    }
+
   }
-  console.log()
+
   document.getElementById("task-board").innerHTML = resultHtml;
 }
 
+function toggleComplete(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
+    }
+  }
+  render()
+}
+function deleteTask(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList.splice(i, 1);
+    }
+  }
+  render()
+}
+function randomIDGenerate() {
+  return '_' + Math.random().toString(36).substring(2, 9);
+}
