aa69997b279de6cd0c5fbf6bdba31d8c4d073cdd
hong-free <hosuye@gmail.com>
2025-09-11 01:34:27 +0900

    Add files via upload

---
+++ todo_list/main.js
@@ -0,0 +1,71 @@
+let taskInput = document.getElementById("task-input");
+let addButton = document.getElementById("add-button");
+let taskList = []
+addButton.addEventListener("click", addTask);
+
+function addTask() {
+
+  let task = {
+    id: randomIDGenerate(),
+    taskContent: taskInput.value,
+    isComplete: false
+  }
+  let taskContent = taskInput.value
+  taskList.push(task)
+  console.log(taskContent)
+  render();
+}
+
+function render() {
+  let resultHtml = '';
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].isComplete == true) {
+      resultHtml +=
+        `<div class="task">
+      <div class="task-done">${taskList[i].taskContent}</div>
+        <div>
+          <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+          <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+        </div >
+        </div > `;
+    } else {
+      resultHtml += `<div class="task">
+      <div>${taskList[i].taskContent}</div>
+        <div>
+          <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+          <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+        </div >
+        </div > `
+    }
+  }
+
+  document.getElementById("task-board").innerHTML = resultHtml;
+}
+function toggleComplete(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
+    }
+  }
+  render();
+}
+function randomIDGenerate() {
+  return "_" + Math.random().toString(36).substring(2, 9);
+}
+
+function deleteTask(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList.splice(i, 1);
+    }
+  } render()
+}
+
+
+
+
+
+
+
+
