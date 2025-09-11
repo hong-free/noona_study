a00a973d356df963307309c247d349a915308c5b
hong-free <hosuye@gmail.com>
2025-09-11 15:32:52 +0900

    2

--- todo_list/main.js
+++ todo_list/main.js
@@ -2,38 +2,57 @@ let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
 let taskList = []
 addButton.addEventListener("click", addTask);
+addButton.addEventListener('click', addTask);
+//입력창에 내용을 넣고 엔터를 치면 내용이 추가된다
+taskInput.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    addTask();
+  }
+});
+
 
 function addTask() {
-
   let task = {
     id: randomIDGenerate(),
     taskContent: taskInput.value,
-    isComplete: false
+    isComplete: false,
   }
   let taskContent = taskInput.value
-  taskList.push(task)
+  if (taskContent.length > 0) {
+    taskList.push(task)
+  }
   console.log(taskContent)
+
+  //할일작성후 입력창 비우고, 입력값이 없을시 알림창 띄우기
+  if (taskContent !== '') {
+    taskInput.value = '';
+    taskInput.focus();
+  } else {
+    alert('할 일을 입력해주세요!');
+  }
   render();
 }
 
+
 function render() {
+
   let resultHtml = '';
   for (let i = 0; i < taskList.length; i++) {
     if (taskList[i].isComplete == true) {
       resultHtml +=
-        `<div class="task">
+        `<div class="task task-bg">
       <div class="task-done">${taskList[i].taskContent}</div>
         <div>
-          <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-          <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+          <button  onclick="toggleComplete('${taskList[i].id}')"> <i class="fa-sharp fa-solid fa-rotate-left"></i></button>
+          <button onclick="deleteTask('${taskList[i].id}')"><i class="fa-solid fa-trash"></i></button>
         </div >
         </div > `;
     } else {
       resultHtml += `<div class="task">
       <div>${taskList[i].taskContent}</div>
         <div>
-          <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-          <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+          <button  onclick="toggleComplete('${taskList[i].id}')"> <i class="fa-solid fa-check"></i></i></button>
+          <button onclick="deleteTask('${taskList[i].id}')"><i class="fa-solid fa-trash"></i></button>
         </div >
         </div > `
     }
@@ -61,11 +80,3 @@ function deleteTask(id) {
     }
   } render()
 }
-
-
-
-
-
-
-
-
