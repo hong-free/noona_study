bb31814d6fddcd2b49698d3acf9aa45ed157b900
hong-free <hosuye@gmail.com>
2025-09-17 15:05:51 +0900

    3

--- newstims/main.js
+++ newstims/main.js
@@ -3,7 +3,8 @@ let news = []
 
 const getLatestNews = async () => {
   // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
-  const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/`);
+  const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`);
+
   const response = await fetch(url)
   const data = await response.json()
   news = data.articles;
