d2d76df66b52e40531f3aa19329027ac7ec52cd9
hong-free <hosuye@gmail.com>
2025-09-21 02:31:54 +0900

    과제14,15,16-1

--- newstims/main.js
+++ newstims/main.js
@@ -7,22 +7,37 @@ let url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlin
 
 const getNews = async () => {
   try {
+    url.searchParams.set("page", page)
+    url.searchParams.set("pageSize", pageSize)
     const response = await fetch(url);
     const data = await response.json();
     if (response.status === 200) {
       if (data.articles.length === 0) {
         throw new Error("no result for this search")
       }
+      if (data.totalResults == 0) {
+        page = 0;
+        totalPage = 0;
+        renderPagination();
+        throw new Error("검색어와 일치하는 결과가 없습니다");
+      }
       newsList = data.articles;
-
+      totalResults = data.totalResults
       render();
+      paginationRender();
 
     } else {
-      throw new Error(data.message)
+      page = 0;
+      totalPage = 0;
+      renderPagination();
+      throw new Error(data.message);
     }
   } catch (error) {
     console.log(error.message)
     errorRender(error.message);
+    page = 0;
+    totalPage = 0;
+    renderPagination();
   }
 }
 
@@ -30,20 +45,23 @@ const getNews = async () => {
 
 
 const getLatestNew = async () => {
+  page = 1
   url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`)
   console.log(url)
   getNews()
 }
-//카테고리로 불로오기
+/*---------카테고리로 불로오기------*/
 const getNewsCategory = async (event) => {
   const category = event.target.textContent.toLowerCase()
+  page = 1
   console.log(category)
   url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${category}`)
   getNews()
 }
 
-//키워드별로 불로오기
+/*----------키워드별로 불로오기-------*/
 const getNewsByKeyword = async () => {
+  page = 1
   const keyword = document.getElementById("search-input").value
 
   console.log(keyword)
@@ -55,7 +73,6 @@ const getNewsByKeyword = async () => {
 
 
 const render = () => {
-
   const newsHTML = newsList.map((news) => `<div class="row news">
           <div class="col-lg-4">
             <img  class="news-img-size" src=
@@ -88,6 +105,82 @@ const errorRender = (errorMessage) => {
 </div>`;
   document.getElementById("news-board").innerHTML = errorHTML
 }
+
+
+
+
+/*----------페이지네이션---------- */
+//totalResult, 토탈결과수
+//page 현재보고있는 페이지
+//pageSize 보여주는 결과를 보여줄지
+//groupSize 페이지 그룹사이즈
+//pageGroup 몇번째 속해있는지
+//lastPage 그룹의 마지막페이지번호
+//firstPage 그룹의 첫번째페이지번호
+//totalPages 그룹의 토탈수
+
+let totalResults = 0
+let page = 1
+const pageSize = 10
+const groupSize = 5
+
+const paginationRender = () => {
+  let paginationHTML = ``;
+  const totalPages = Math.ceil(totalResults / pageSize)
+  const pageGroup = Math.ceil(page / groupSize)//pageGroup 몇번째 그룹속해있는지
+
+  // let last = pageGroup * 5; // lastPage 그룹의 마지막페이지번호
+  // if (last > totalPages) {
+  //   last = totalPages;
+  // }
+  // let first = last - 4 < 0 ? 1 : last - 4;
+
+  let lastPage = pageGroup * groupSize
+  if (lastPage > totalPages) {
+    lastPage = totalPages
+  }
+
+  const firstPage = lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
+
+
+  if (page > 1) {
+    paginationHTML = `
+  <li class="page-item" onclick="moveToPage(1)">
+                        <a class="page-link" href='#js-bottom'>&lt;&lt;</a>
+                      </li>
+                      <li class="page-item" onclick="moveToPage(${page - 1})">
+                        <a class="page-link" href='#js-bottom'>&lt;</a>
+                      </li>`;
+  }
+  for (let i = firstPage; i <= lastPage; i++) {
+
+    paginationHTML += `
+    <li class="page-item ${i === page ? 'active' : ''}" onclick="moveToPage(${i})"><a class="page-link" >${i}</a></li>`
+  }
+  if (page < totalPages) {
+
+
+    paginationHTML += ` 
+    <li class="page-item" onclick="moveToPage(${page + 1})">
+     <a  class="page-link" href='#js-program-detail-bottom'>&gt;</a>
+                       </li>
+<li class="page-item" onclick="moveToPage(${totalPages})">
+                        <a class="page-link" href='#js-bottom'>&gt;&gt;</a>
+                       </li>`
+
+  }
+  document.querySelector(".pagination").innerHTML = paginationHTML
+}
+const moveToPage = (pageNum) => {
+  page = pageNum
+  getNews()
+  console.log(pageNum)
+}
+
+
+
+
+
 
 
 getLatestNew()
@@ -127,8 +220,6 @@ trigger.addEventListener("click", () => {
   trigger.classList.toggle("active")
   menusAll.classList.toggle("active")
 });
-
-
 
 
 
